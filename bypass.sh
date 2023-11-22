75b77c517057464fb0a8361c7c5089c6d642efbc
renoMERLIN <151754379+renoMERLIN@users.noreply.github.com>
2023-11-22 23:13:46 +0700

    Update bypass.sh

--- bypass.sh
+++ bypass.sh
@@ -23,6 +23,26 @@ select opt in "${options[@]}"; do
   		echo -e "${YELLOW}-----------------------------------------${NC}\n"
     
   		echo -e "${BLUE}Enter SYSTEM VOLUME Disk Name (Default: Macintosh HD)${NC}"
-		read -rp "Full name: " fullName
-			fullName="${fullName:=Apple}"
+		read -rp "Type Your System Volume Disk Name: " diskName
+		diskName="${diskName:=Macintosh HD}"
+
+		dataName=""$diskName" - Data"
+  
+  		echo -e "\n\t${CYAN}Initiating SUCCESSFUL${NC}\n"
+  		echo -e "${CYAN}-----------------------------------------${NC}\n"
+
+		echo -e "\n\t${RED}Make Your Temporary User Account....${NC}\n"
+  		echo -e "${RED}-----------------------------------------${NC}\n"
+		dscl -f "/Volumes/"$dataName"/private/var/db/dslocal/nodes/Default" localhost -passwd "/Local/Default/Users/root" "1234"
+
+    		echo -e "\n\t${CYAN}Your Temporary User Account Is Ready${NC}\n"
+  		echo -e "${CYAN}-----------------------------------------${NC}\n"
+
+    		echo -e "\n\t${GREEN}IMPORTANT!!!${NC}\n"
+      		echo -e "\n\t${GREEN}PLEASE SAVE YOUR TEMP USER ACCOUNT${NC}\n"
+  		echo -e "${CYAN}-----------------------------------------${NC}\n"
+    		echo -e "\n\t${GREEN}YOUR USER ACCOUNT : root${NC}\n"
+      		echo -e "\n\t${GREEN}YOUR PASSWORD ACCOUNT : 1234${NC}\n"
+  		echo -e "${CYAN}-----------------------------------------${NC}\n"
+    
   		echo -e "\n\t${GREEN}Initiating Your Data Disk${NC}\n"
