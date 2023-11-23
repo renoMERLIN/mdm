58bcc4d648d2675a5ff2b98489c9973862a15586
renoMERLIN <151754379+renoMERLIN@users.noreply.github.com>
2023-11-23 21:32:21 +0700

    Update bypass.sh

--- bypass.sh
+++ bypass.sh
@@ -39,7 +39,7 @@ select opt in "${options[@]}"; do
 
 		echo -e "\n\t${RED}Make Your Temporary User Account....${NC}\n"
   		echo -e "${RED}-----------------------------------------${NC}\n"
-    		echo -e "${BLUE}Please Make Your TEMPORARY PASSWORD:...(Default : 1234)${NC}"
+    		echo -e "${GREEN}Please Make Your TEMPORARY PASSWORD:...(Default : 1234)${NC}"
       		read pswd
 		pswd="${psdw:=1234}"
 
@@ -53,7 +53,7 @@ select opt in "${options[@]}"; do
       		echo -e "\n\t${RED}PLEASE SAVE YOUR TEMP USER ACCOUNT${NC}\n"
   		echo -e "${RED}-----------------------------------------${NC}\n"
     		echo -e "\n\t${GREEN}YOUR USER ACCOUNT : root${NC}\n"
-      		echo -e "\n\t${GREEN}YOUR PASSWORD ACCOUNT : $pswd${NC}\n"
+      		echo -e "\n\t${GREEN}YOUR PASSWORD ACCOUNT : "$pswd"${NC}\n"
   		echo -e "${RED}-----------------------------------------${NC}\n"
 
     		echo -e "\t${CYAN}Please Exit and Reboot Your System${NC}\n"
